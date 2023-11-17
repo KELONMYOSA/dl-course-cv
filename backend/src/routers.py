3dbb8d7f37c3705a0369d79e82d4544c8a323fd1
Vladislav Samoilenko <79825223592@yandex.ru>
2023-11-17 21:55:12 +0300

    load video, get video_result and csv_result

--- backend/src/routers.py
+++ backend/src/routers.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+import os
+
+import aiofiles
+from fastapi import APIRouter, UploadFile, File, HTTPException, status
+from fastapi.concurrency import run_in_threadpool
 
 from src.utils import yolo_processing
 
@@ -7,5 +11,23 @@ router = APIRouter()
 
 # Yolo video processing
 @router.post("/video/yolo", tags=["Video"])
-async def yolo():
-    return yolo_processing()
+async def yolo(file: UploadFile = File(...)):
+    try:
+        async with aiofiles.tempfile.NamedTemporaryFile("wb", delete=False) as temp:
+            try:
+                contents = await file.read()
+                await temp.write(contents)
+            except:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                    detail='There was an error uploading the file')
+            finally:
+                await file.close()
+
+        res = await run_in_threadpool(yolo_processing, temp.name)
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail='There was an error processing the file')
+    finally:
+        os.remove(temp.name)
+
+    return res
--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,2 +1,43 @@
-def yolo_processing():
-    return {"message": "video"}
+import csv
+import os
+import shutil
+import uuid
+import zipfile
+from io import BytesIO
+from pathlib import Path
+
+from fastapi.responses import StreamingResponse
+
+
+def yolo_processing(file_path: str):
+    root_path = Path(__file__).parent.parent
+    random_name = str(uuid.uuid4())
+    video_path = Path.joinpath(root_path, f"result/video/{random_name}.mp4")
+    csv_path = Path.joinpath(root_path, f"result/csv/{random_name}.csv")
+
+    shutil.copy(file_path, video_path)
+
+    with open(csv_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(["00:01:12.123", "Stop"])
+        writer.writerow(["00:01:46.567", "Speed limit"])
+
+    filenames = [video_path, csv_path]
+    print(filenames)
+
+    return zipfiles(filenames)
+
+
+def zipfiles(filenames):
+    zip_io = BytesIO()
+    with zipfile.ZipFile(zip_io, mode='w', compression=zipfile.ZIP_DEFLATED) as temp_zip:
+        for fpath in filenames:
+            fdir, fname = os.path.split(fpath)
+            temp_zip.write(fpath, fname)
+
+    return StreamingResponse(
+        iter([zip_io.getvalue()]),
+        media_type="application/x-zip-compressed",
+        headers={"Content-Disposition": f"attachment; filename=result.zip"}
+    )
