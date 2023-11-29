06875ffb5ca847e752eee88140f5ce54b377d8e6
Vladislav Samoilenko <79825223592@yandex.ru>
2023-11-29 23:02:01 +0300

    avoiding IO

--- backend/src/yolo.py
+++ backend/src/yolo.py
@@ -1,16 +1,18 @@
 import json
 import logging
-import tempfile
-import uuid
-from os import path
+import time
 
 import cv2
-from aiofiles import os
+import numpy
+import torch
 from ultralytics import YOLO
 
 logging.getLogger("ultralytics").setLevel(logging.ERROR)
 
 model = YOLO("storage/weights/120_epochs.pt")
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+model.to(device)
+
 with open("storage/maps/our_signs_map.json") as json_file:
     signs_map = json.load(json_file)
 with open("storage/maps/text_info_mapping.json") as json_file:
@@ -19,8 +21,8 @@ with open("storage/maps/high_priority_mapping.json") as json_file:
     priority_map = json.load(json_file)
 
 
-def _predict_img(path: str):
-    result = model(path, conf=0.5, imgsz=1280)
+def _predict_img(img: numpy.ndarray):
+    result = model(img, conf=0.5, imgsz=1280, device=device)
     return result[0]
 
 
@@ -34,12 +36,13 @@ def priority_flag(sign_name: str) -> int:
 async def predict_video(
     input_video_path: str, out_video_path: str, logs_file, frames_in_row: int = 10, frame_slip: int = 5
 ):
-    tmp_dir = tempfile.gettempdir()
+    time_start = time.time()
+
     cap = cv2.VideoCapture(input_video_path)
 
     video_fps = cap.get(cv2.CAP_PROP_FPS)
     video_shape = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    out_video_frames = []
+    out = cv2.VideoWriter(out_video_path, cv2.VideoWriter_fourcc(*"vp80"), video_fps, video_shape)
 
     frame_count = 0
     detected_signs = {}
@@ -53,12 +56,8 @@ async def predict_video(
 
         frame_count += 1
 
-        tmp_frame_img = path.join(tmp_dir, f"{uuid.uuid1()!s}.jpg")
-        cv2.imwrite(tmp_frame_img, frame)
-        res = _predict_img(tmp_frame_img)
-        await os.remove(tmp_frame_img)
-
-        out_video_frames.append(res.plot())
+        res = _predict_img(frame)
+        out.write(res.plot())
 
         names = []
         detection_count = res.boxes.shape[0]
@@ -94,7 +93,8 @@ async def predict_video(
 
     cap.release()
 
-    out = cv2.VideoWriter(out_video_path, cv2.VideoWriter_fourcc(*"vp80"), video_fps, video_shape)
-
-    for frame in out_video_frames:
-        out.write(frame)
+    print("Task completed")
+    print(f"Video duration: {frame_count / video_fps} seconds")
+    print(f"FPS: {video_fps}")
+    print(f"Task completion time: {time.time() - time_start} seconds")
+    print("--------------------------------------------------")
